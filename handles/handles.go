b8c5e15a8afa3ebfacc3e1273a23c7a754948f26
antojama <a.martinez@westorage.cl>
2025-04-29 14:20:57 -0400

    v1.0.6 correccion handles

--- handles/handles.go
+++ handles/handles.go
@@ -1,6 +1,7 @@
 package handles
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -17,24 +18,19 @@ func HandleToken(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Leer el cuerpo de la solicitud
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error al leer el cuerpo de la solicitud", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-
-	// Decodificar el cuerpo JSON
 	var tokenRequest models.TokenRequest
-	if err := json.Unmarshal(body, &tokenRequest); err != nil {
-		http.Error(w, "Error al decodificar el JSON", http.StatusBadRequest)
-		return
-	}
+	body, _ := io.ReadAll(r.Body)
+	r.Body = io.NopCloser(bytes.NewBuffer(body)) // Reusar el body para r.FormValue
 
-	// Validar que el token no esté vacío
-	if tokenRequest.Token == "" {
-		http.Error(w, "El token es requerido", http.StatusBadRequest)
-		return
+	if err := json.Unmarshal(body, &tokenRequest); err != nil || tokenRequest.Token == "" {
+		// Si falla, probamos leerlo desde el formulario (x-www-form-urlencoded)
+		r.ParseForm()
+		token := r.FormValue("token")
+		if token == "" {
+			http.Error(w, "Token no encontrado en JSON ni en formulario", http.StatusBadRequest)
+			return
+		}
+		tokenRequest.Token = token
 	}
 
 	// Aquí puedes llamar a tu función principal con el token recibido
